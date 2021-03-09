e546cea848a15bda6aedbe2fb02161ec3e164f6d
cyn <1756538768@qq.com>
2021-03-09 17:14:27 +0800

    refactor: refactor layout as grid.

--- renderer-process/layout.js
+++ renderer-process/layout.js
@@ -2,18 +2,12 @@ function updateAppLayout() {
     var length = document.getElementById("selected-files").getElementsByTagName("li").length
     if (length > 0) {
         document.getElementById("welcome").style.display = "none"
-        document.getElementById("sidepanel-bar").style.display = ""
         document.getElementById("sidepanel").style.display = ""
-        document.getElementById("viewer-bar").style.display = ""
         document.getElementById("viewer").style.display = ""
-        document.getElementById("viewerpanel").style.display = ""
     } else {
         document.getElementById("welcome").style.display = ""
-        document.getElementById("sidepanel-bar").style.display = "none"
         document.getElementById("sidepanel").style.display = "none"
-        document.getElementById("viewer-bar").style.display = "none"
         document.getElementById("viewer").style.display = "none"
-        document.getElementById("viewerpanel").style.display = "none"
     }
 }
 
